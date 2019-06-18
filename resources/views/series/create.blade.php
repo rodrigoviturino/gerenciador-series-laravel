66a8d343cdbb4ced89be1a412018b771c8ff2bcc
Rodrigo Viturino <viturino_souza@outlook.com>
2019-06-18 01:33:56 -0300

    Criando o model com SQLite

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -11,11 +11,7 @@
       <label for="nome">Nome</label>
       <input type="text" class="form-control" id="nome" placeholder="Insira o nome">
     </div>
-    <div class="form-group">
-      <label for=""></label>
-      <input type="text" class="form-control" id="" placeholder="">
-    </div>
-    
+       
     <button type="submit" class="btn btn-primary">Adicionar</button>
   </form>
 
